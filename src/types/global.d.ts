eba420a5c57e1c526f7d3e18504cb2ec544138cb
hanyongjun <gksdydwns34@naver.com>
2024-02-03 01:20:16 +0900

    이미지 파일 별 형식 선언

---
+++ src/types/global.d.ts
@@ -0,0 +1,10 @@
+//이미지 형식 별 타입 정의
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.webp";
+
+declare module "*.css"{
+    const content: {[className: string]: string};
+    export = content; 
+}
