8dac0fe119b5b2a858ed561c7fcdbfb2a636f5fc
hanyongjun <gksdydwns34@naver.com>
2024-02-02 19:09:34 +0900

    메인 페이지 디자인 작업(prototype)완성

--- src/component/main.tsx
+++ src/component/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Art from "../img/art.png";
+import Arrow from "../img/arrow.png";
 import "../scss/main.scss";
 
 function Main() {
@@ -7,11 +8,15 @@ function Main() {
     <div className="main">
         <div className="main_banner">
             <img src={Art}/>
-            <p>
+        </div>
+        <p className='main_title'>
                 자신의 그림을 분석해보고<br/>
                 결과를 확인해보세요
-            </p>
-        </div>
+        </p>
+      
+        <img src={Arrow} className='arrow'/>
+      <button className='start_btn'>분석하기</button>
+
     </div>
   );
 }
