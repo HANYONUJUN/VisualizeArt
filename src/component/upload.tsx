c0160ea6be2db7c15564fab7a9bcc847f9ad1e21
hanyongjun <gksdydwns34@naver.com>
2024-02-08 17:13:16 +0900

    image업로드 구현

--- src/component/upload.tsx
+++ src/component/upload.tsx
@@ -1,19 +1,46 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { useState } from 'react';
 import '../scss/upload.scss';
 
-class Upload extends Component {
 
+export default function Upload() {
 
+  const [imageSrc, setImageSrc]: any = useState(null);
 
+  const onUpload = (e:any) => {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
 
-    render() {
-        return(
+      return new Promise<void>((resolve)=>{
+        reader.onload= () => {
+          setImageSrc(reader.result || null); //파일의 컨텐츠
+          resolve();
+        };
+    });
+  }
+
+  return(
           <>
-            <div className='upload_image_view'></div>
-           
+            <div className='upload_image_view'
+                style={{
+                  backgroundImage: `url(${imageSrc})`,
+                  backgroundSize: 'contain',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'center',
+                }}
+             ></div>
+            
+
+            <label className="input-file-button" for="input-file">upload</label>
+            <input
+              accept='image/*'
+              multiple type="file"
+              id="input-file"
+              onChange={e => onUpload(e)}
+              style={{display:'none'}}
+            />         
           </>
         )
-    }
-};
+    };
 
-export default Upload;
